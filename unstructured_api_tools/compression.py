ddb0bd936ea775fcbffd145f6b2b47d4dacf0a74
Matt Robinson <mrobinson@unstructuredai.io>
2022-12-01 18:29:39 -0500

    added functions for expanding zipped and tarred lists

--- unstructured_api_tools/compression.py
+++ unstructured_api_tools/compression.py
@@ -1,6 +1,32 @@
 import tarfile
 import tempfile
+from typing import List
 import zipfile
+
+from fastapi import UploadFile
+
+
+def expand_tarred_upload(upload_file: UploadFile) -> List[UploadFile]:
+    files: List[UploadFile] = list()
+
+    tar = upload_file.file
+    with tar.open(tar.name, "r:*") as _tar:
+        for member in _tar.getmembers():
+            f = _tar.extractfile(member)
+            files.append(UploadFile(filename=f.name, file=f))
+
+    return files
+
+
+def expand_zipped_upload(upload_file: UploadFile) -> List[UploadFile]:
+    files: List[UploadFile] = list()
+
+    zipped_file = upload_file.file
+    for filename in zipped_file.namelist():
+        f = zipped_file.open(filename)
+        files.append(UploadFile(filename=f.name, file=f))
+
+    return files
 
 
 def is_tarfile(file: tempfile.SpooledTemporaryFile) -> bool:
