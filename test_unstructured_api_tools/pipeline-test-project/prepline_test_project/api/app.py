5b065524b1090886687e9ddae7c947bd303d7374
kravetsmic <kravets.mic@gmail.com>
2023-03-09 10:26:21 +0200

    updated text-file notebooks, added endpoints per notebook to test api

--- test_unstructured_api_tools/pipeline-test-project/prepline_test_project/api/app.py
+++ test_unstructured_api_tools/pipeline-test-project/prepline_test_project/api/app.py
@@ -7,6 +7,18 @@
 from fastapi import FastAPI, Request, status
 
 from .process_file_1 import router as process_file_1_router
+from .process_file_2 import router as process_file_2_router
+from .process_file_3 import router as process_file_3_router
+from .process_file_4 import router as process_file_4_router
+from .process_file_5 import router as process_file_5_router
+from .process_text_1 import router as process_text_1_router
+from .process_text_2 import router as process_text_2_router
+from .process_text_3 import router as process_text_3_router
+from .process_text_4 import router as process_text_4_router
+from .process_text_file_1 import router as process_text_file_1_router
+from .process_text_file_2 import router as process_text_file_2_router
+from .process_text_file_3 import router as process_text_file_3_router
+from .process_text_file_4 import router as process_text_file_4_router
 
 
 app = FastAPI(
@@ -16,6 +28,18 @@ app = FastAPI(
 )
 
 app.include_router(process_file_1_router)
+app.include_router(process_file_2_router)
+app.include_router(process_file_3_router)
+app.include_router(process_file_4_router)
+app.include_router(process_file_5_router)
+app.include_router(process_text_1_router)
+app.include_router(process_text_2_router)
+app.include_router(process_text_3_router)
+app.include_router(process_text_4_router)
+app.include_router(process_text_file_1_router)
+app.include_router(process_text_file_2_router)
+app.include_router(process_text_file_3_router)
+app.include_router(process_text_file_4_router)
 
 
 @app.get("/healthcheck", status_code=status.HTTP_200_OK)
