313bb533a016cb01968c14528fbd69256d3b3209
Austin Walker <austin@unstructured.io>
2023-03-19 11:01:54 -0400

    Run linter

--- test_unstructured_api_tools/api/test_file_apis.py
+++ test_unstructured_api_tools/api/test_file_apis.py
@@ -111,22 +111,30 @@ def test_process_file_1(test_files, test_params, test_type_header, expected_stat
     "mimetype,filename,expected_status",
     [
         ("application/epub+zip", "file.epub", 200),
-        ("application/json",     "file.json", 200),
-        ("application/msword",   "file.doc",  200),
-        ("application/pdf",      "file.pdf",  200),
+        ("application/json", "file.json", 200),
+        ("application/msword", "file.doc", 200),
+        ("application/pdf", "file.pdf", 200),
         ("application/vnd.ms-powerpoint", "file.ppt", 200),
-        ("application/vnd.openxmlformats-officedocument.presentationml.presentation", "file.pptx", 200),
-        ("application/vnd.openxmlformats-officedocument.wordprocessingml.document",   "file.docx", 200),
-        ("application/xml",      "file.xml",  400),
-        ("application/zip",      "file.zip",  400),
-        ("image/jpeg",           "file.jpeg", 200),
-        ("image/png",            "file.png",  200),
-        ("message/rfc822",       "file.eml",  200),
-        ("text/html",            "file.html", 200),
-        ("text/markdown",        "file.md",   200),
-        ("text/plain",           "file.txt",  200),
-        ("video/mp4",            "file.mp4",  400),
-    ]
+        (
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+            "file.pptx",
+            200,
+        ),
+        (
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "file.docx",
+            200,
+        ),
+        ("application/xml", "file.xml", 400),
+        ("application/zip", "file.zip", 400),
+        ("image/jpeg", "file.jpeg", 200),
+        ("image/png", "file.png", 200),
+        ("message/rfc822", "file.eml", 200),
+        ("text/html", "file.html", 200),
+        ("text/markdown", "file.md", 200),
+        ("text/plain", "file.txt", 200),
+        ("video/mp4", "file.mp4", 400),
+    ],
 )
 def test_supported_mimetypes(mimetype, filename, expected_status):
     """
@@ -141,22 +149,27 @@ def test_supported_mimetypes(mimetype, filename, expected_status):
     response = client.post(
         PROCESS_FILE_1_ROUTE, files=[("files", ("example-filename", open(FILE_A, "rb"), mimetype))]
     )
-    assert(response.status_code == expected_status)
+    assert response.status_code == expected_status
 
     # Multiple files
     response = client.post(
-        PROCESS_FILE_1_ROUTE, files=[("files", ("example-filename", open(FILE_A, "rb"), mimetype)),
-                                     ("files", ("example-filename", open(FILE_A, "rb"), mimetype))]
+        PROCESS_FILE_1_ROUTE,
+        files=[
+            ("files", ("example-filename", open(FILE_A, "rb"), mimetype)),
+            ("files", ("example-filename", open(FILE_A, "rb"), mimetype)),
+        ],
     )
 
-    # When there are more test fixtures - come back and add single file plus text input, multi file plus text input
+    # When there are more test fixtures - come back
+    # and add single file plus text input, multi file plus text input
 
     # If the client doesn't set a mimetype, we may just see application/octet-stream
     # Here we fall back to the file extension
     response = client.post(
-        PROCESS_FILE_1_ROUTE, files=[("files", (filename, open(FILE_A, "rb"), "application/octet-stream"))]
+        PROCESS_FILE_1_ROUTE,
+        files=[("files", (filename, open(FILE_A, "rb"), "application/octet-stream"))],
     )
-    assert(response.status_code == expected_status)
+    assert response.status_code == expected_status
 
 
 def test_reconfigure_supported_mimetypes():
@@ -166,18 +179,21 @@ def test_reconfigure_supported_mimetypes():
     client = TestClient(app)
 
     response = client.post(
-        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))]
+        PROCESS_FILE_1_ROUTE,
+        files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))],
     )
-    assert(response.status_code == 200)
+    assert response.status_code == 200
 
     os.environ["UNSTRUCTURED_ALLOWED_MIMETYPES"] = "image/jpeg"
 
     response = client.post(
-        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))]
+        PROCESS_FILE_1_ROUTE,
+        files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))],
     )
-    assert(response.status_code == 400)
+    assert response.status_code == 400
 
     response = client.post(
-        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_B, open(FILE_B, "rb"), FILENAME_FORMATS[FILE_B]))]
+        PROCESS_FILE_1_ROUTE,
+        files=[("files", (FILE_B, open(FILE_B, "rb"), FILENAME_FORMATS[FILE_B]))],
     )
-    assert(response.status_code == 200)
+    assert response.status_code == 200
