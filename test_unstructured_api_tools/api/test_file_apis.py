cc773d93e6c64cb81747efbc85dce8f9ec63eed1
Austin Walker <austin@unstructured.io>
2023-03-19 09:57:33 -0400

    Add tests for supported mimetypes

--- test_unstructured_api_tools/api/test_file_apis.py
+++ test_unstructured_api_tools/api/test_file_apis.py
@@ -1,5 +1,6 @@
 from base64 import b64decode
 import json
+import os
 import pytest
 
 from requests_toolbelt.multipart import decoder
@@ -15,7 +16,7 @@ FILE_B = "test_unstructured_api_tools/api/fixtures/example.jpg"
 
 FILENAME_LENGTHS = {FILE_A: 36602, FILE_B: 32764}
 FILENAME_FORMATS = {
-    FILE_A: "application/vnd.openxmlformats",
+    FILE_A: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     FILE_B: "image/jpeg",
 }
 
@@ -104,3 +105,79 @@ def test_process_file_1(test_files, test_params, test_type_header, expected_stat
         else:
             assert response.headers["content-type"] == test_type_header
         _assert_response_for_process_file_1(test_files, test_params, test_type_header, response)
+
+
+@pytest.mark.parametrize(
+    "mimetype,filename,expected_status",
+    [
+        ("application/epub+zip", "file.epub", 200),
+        ("application/json",     "file.json", 200),
+        ("application/msword",   "file.doc",  200),
+        ("application/pdf",      "file.pdf",  200),
+        ("application/vnd.ms-powerpoint", "file.ppt", 200),
+        ("application/vnd.openxmlformats-officedocument.presentationml.presentation", "file.pptx", 200),
+        ("application/vnd.openxmlformats-officedocument.wordprocessingml.document",   "file.docx", 200),
+        ("application/xml",      "file.xml",  400),
+        ("application/zip",      "file.zip",  400),
+        ("image/jpeg",           "file.jpeg", 200),
+        ("image/png",            "file.png",  200),
+        ("message/rfc822",       "file.eml",  200),
+        ("text/html",            "file.html", 200),
+        ("text/markdown",        "file.md",   200),
+        ("text/plain",           "file.txt",  200),
+        ("video/mp4",            "file.mp4",  400),
+    ]
+)
+def test_supported_mimetypes(mimetype, filename, expected_status):
+    """
+    Verify that we return 400 if a filetype is not supported.
+    Because we use the mimetype for this, let's just POST different content types
+    without caring what the actual file is.
+    """
+    client = TestClient(app)
+
+    # Keying off of the mimetype just works (there are 4 paths to test)
+    # One file
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", ("example-filename", open(FILE_A, "rb"), mimetype))]
+    )
+    assert(response.status_code == expected_status)
+
+    # Multiple files
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", ("example-filename", open(FILE_A, "rb"), mimetype)),
+                                     ("files", ("example-filename", open(FILE_A, "rb"), mimetype))]
+    )
+
+    # When there are more test fixtures - come back and add single file plus text input, multi file plus text input
+
+    # If the client doesn't set a mimetype, we may just see application/octet-stream
+    # Here we fall back to the file extension
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", (filename, open(FILE_A, "rb"), "application/octet-stream"))]
+    )
+    assert(response.status_code == expected_status)
+
+
+def test_reconfigure_supported_mimetypes():
+    """
+    Verify that we can change the supported mimetype list via env variable
+    """
+    client = TestClient(app)
+
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))]
+    )
+    assert(response.status_code == 200)
+
+    os.environ["UNSTRUCTURED_ALLOWED_MIMETYPES"] = "image/jpeg"
+
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_A, open(FILE_A, "rb"), FILENAME_FORMATS[FILE_A]))]
+    )
+    assert(response.status_code == 400)
+
+    response = client.post(
+        PROCESS_FILE_1_ROUTE, files=[("files", (FILE_B, open(FILE_B, "rb"), FILENAME_FORMATS[FILE_B]))]
+    )
+    assert(response.status_code == 200)
